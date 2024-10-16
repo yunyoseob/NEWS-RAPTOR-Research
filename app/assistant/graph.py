cbe39c2dbfb20739592a3789c813b18e54072800
yunyoseob <imyoseob@naver.com>
2024-10-17 03:22:47 +0900

    graph image add

--- app/assistant/graph.py
+++ app/assistant/graph.py
@@ -3,6 +3,13 @@ from functools import lru_cache
 from langgraph.graph import END, StateGraph
 from app.assistant.state import AgentState
 from app.assistant.nodes import (Search_type, RetrieveDocuments, RunPrompt)
+from PIL import Image as PILImage
+
+from io import BytesIO
+import nest_asyncio
+from IPython.display import display
+from langchain_core.runnables.graph import CurveStyle, MermaidDrawMethod, NodeStyles
+from PIL import Image as PILImage
 
 @lru_cache
 def CompilingGraph():
@@ -24,3 +31,24 @@ def CompilingGraph():
     app = graph.compile()
     
     return app
+
+if __name__ == "__main__":
+    # Compile the graph
+    graph = CompilingGraph()
+
+    nest_asyncio.apply()  # Required for Jupyter Notebook to run async functions
+
+    png_data = graph.get_graph().draw_mermaid_png(
+                curve_style=CurveStyle.LINEAR,
+                node_colors=NodeStyles(first="#ffdfba", last="#baffc9", default="#fad7de"),
+                wrap_label_n_words=9,
+                output_file_path=None,
+                draw_method=MermaidDrawMethod.PYPPETEER,
+                background_color="white",
+                padding=10,
+            )
+    with open("graph.png", "wb") as f:
+        f.write(png_data)  # png_data should now contain the binary image data
+    # Optionally display the image using PIL
+    img = PILImage.open("graph.png")
+    img.show()
